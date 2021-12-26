333b5d012b3c61be23ef3dd46b4cb961a322fdeb
adliazamuddin <adliazamuddin@gmail.com>
2021-12-26 22:52:30 +0800

    added temporary

--- 100day/day1/helloworld.py
+++ 100day/day1/helloworld.py
@@ -2,3 +2,4 @@
 print("hello world")
 name = input("WhAT IS YOUr name")
 print(name)
+
---
+++ 100day/day1/temporaryVariable.py
@@ -0,0 +1,10 @@
+
+a = input("Enter a: \n")
+b = input("Enter b: \n")
+
+c = a 
+a = b 
+b = c
+
+print("a : " +a)
+print("b : " +b)
