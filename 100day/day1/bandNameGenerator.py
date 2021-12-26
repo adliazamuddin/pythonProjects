36853d0700b177a00e1c8545ff71e6257f1311fe
adliazamuddin <adliazamuddin@gmail.com>
2021-12-26 22:46:05 +0800

    added new file

---
+++ 100day/day1/bandNameGenerator.py
@@ -0,0 +1,11 @@
+#Create greeting
+print("Welcome to the band name generator.")
+
+#Ask user for the city
+city = input("Which city did you grow up in?\n")
+
+#Ask user the name of pet
+pet = input("What is the name of a pet?\n")
+
+#Combine the input
+print("Your band name could be " + city + " " + pet)
